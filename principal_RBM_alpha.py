14669a8896cecfd69e8e4676749878b8d22c2468
Ahmed-Yassine Chraa <Ahmedyassine2002@gmail.com>
2025-03-15 02:10:08 +0100

    cleaning code

--- principal_RBM_alpha.py
+++ principal_RBM_alpha.py
@@ -2,9 +2,8 @@ import numpy as np
 import scipy.io
 from math import *
 import torch
-from torch import from_numpy as torchnp
-from torch import bernoulli
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 path_data = "data/binaryalphadigs.mat"
@@ -39,93 +38,83 @@ class RBM:
         pass    
 
     def init_RBM(self, p, q): 
-        self.p = p # Nombre de neurones d'entrée => doit être le même que taille des données, à vérifier lors de l'entraînement
-        self.q = q  # hyperparameters 
-        self.b = torchnp(np.zeros((1, self.q))).to(device)
-        self.a = torchnp(np.zeros((1, self.p))).to(device)
-        self.W = torchnp(np.random.normal(0, 0.01, size=(self.p, self.q))).to(device)
+        self.p = p
+        self.q = q  # ( to fine-tune )
+        self.b = torch.zeros((1, self.q), dtype=torch.double, device=device)
+        self.a = torch.zeros((1, self.p), dtype=torch.double, device=device)
+        self.W = torch.normal(0, 0.01, size=(self.p, self.q), device=device, dtype=torch.double)
 
-    def entree_sortie_RBM(self, donnees_entree, sig=True):
-        if donnees_entree.shape[1]!= 1:
-            raise ValueError("Erreur : 'donnees_entree' n'est pas de la bonne dimension (n, 1, p).")
-        if sig is True:
-            res = torch.sigmoid(donnees_entree @ self.W + self.b)
+    def entree_sortie_RBM(self, X_H, sigmoid=True):
+        if sigmoid :
+            return torch.sigmoid(X_H @ self.W + self.b)
         else :
-            res = donnees_entree @ self.W + self.b
-        return res
-
+            return X_H @ self.W + self.b
+        
     def sortie_entree_RBM(self, donnees_sortie):
         return torch.sigmoid(donnees_sortie @ torch.transpose(self.W, 0, 1) + self.a)
     
-    def train_RBM(self, epochs, lr, batch_fraction, x, plot_error=False) :
+    def train_RBM(self, epochs, lr, batch_fraction, x, verbose=False) :
         # vérifier type de x et le mette en torch
         if type(x) == np.ndarray:
-            x = torchnp(x).to(device)
+            x = torch.from_numpy(x).to(device)
             x = x.double()
         x = x.to(device)
-        print(f"Taille des données ====> {x.shape}")
         assert 0 < batch_fraction <= 1
         batch_size = max(1, int(np.floor(batch_fraction * x.shape[0])))
-        print(f"Batch size ====> {batch_size}")
         # ////!\\\\ x doit être de la taille (n, 1, p)
         if self.p != x.shape[2]:
             raise ValueError("p doit être égal à la taille d'une donnée d'entrée (nombre de pixels).")
         # à répéter pour chaque epoch
-        eqm_list = []
-        for e in range(epochs):  
-            v_0 = x # données d'entrees  ////!\\\\ (n, 1, p) n en première dimension !!!
-            # Batch size 
-            shuffled_indices = np.arange(v_0.shape[0])
-            np.random.shuffle(shuffled_indices)
-            v_0_shuffled = v_0[shuffled_indices]
-            num_batches = np.ceil(v_0.shape[0] / batch_size).astype(int)
-            list_v_0 = [v_0_shuffled[i * batch_size:(i + 1) * batch_size] for i in range(num_batches)]
-            for v_0 in list_v_0 :
-                # Forward v0 -> h0
-                tirage_h = self.entree_sortie_RBM(v_0)  # (n, 1, q)
-                # Tirage de h0 (taille q x 1) dans p(h|v), tirer q nombres entre 0 et 1 et comparer à h
-                h = bernoulli(tirage_h)
-                # Backward h0 -> v1
-                tirage_v = self.sortie_entree_RBM(h)  # (n, 1, p)
-                # Tirage de v1 (taille p x 1) dans loi p(v|h^0)
-                v_1 = bernoulli(tirage_v)
-                # Forward v1 -> h1
-                tirage_h_1 =self.entree_sortie_RBM(v_1)  # (n, 1, q)
+        acc_list = []
+        with tqdm(total=epochs, desc="Training RBM", unit="epoch") as pbar:
+            for e in range(epochs):
+            # for e in tqdm(range(epochs), desc="Training RBM", unit="epoch"):  
+                v0 = x # données d'entrees  ////!\\\\ (n, 1, p) n en première dimension !!!
+                shuffled_indices = np.arange(v0.shape[0])
+                np.random.shuffle(shuffled_indices)
+                v_0_shuffled = v0[shuffled_indices]
+                num_batches = np.ceil(v0.shape[0] / batch_size).astype(int)
+                list_v_0 = [v_0_shuffled[i * batch_size:(i + 1) * batch_size] for i in range(num_batches)]
+                for v0 in list_v_0 :
+                    P_h_v0 = self.entree_sortie_RBM(v0)  # (n, 1, q)
+                    h0 = torch.bernoulli(P_h_v0)
+                    p_v_h0 = self.sortie_entree_RBM(h0)  
+                    v1 = torch.bernoulli(p_v_h0)
+                    p_h_v1 = self.entree_sortie_RBM(v1)  # (n, 1, q)
+                    delta_W = torch.transpose(v0, 1, 2) @ p_h_v1 - torch.transpose(v1, 1, 2) @ p_h_v1  # (p, 1, n) * (1, q, n) = (p, q, n)
+                    delta_a = v0 - v1  # (n, 1, p) 
+                    delta_b = P_h_v0 - p_h_v1 # (n ,1, q)
 
-                delta_W = torch.transpose(v_0, 1, 2) @ tirage_h - torch.transpose(v_1, 1, 2) @ tirage_h_1  # (p, 1, n) @ (1, q, n) => (p, q, n)
-                delta_a = v_0 - v_1  # (n, 1, p) #v_1
-                delta_b = tirage_h - tirage_h_1  # (n ,1, q)
+                    delta_W = delta_W.mean(axis=0)
+                    delta_a = delta_a.mean(axis=0)
+                    delta_b = delta_b.mean(axis=0)
 
-                delta_W = delta_W.mean(axis=0)
-                delta_a = delta_a.mean(axis=0)
-                delta_b = delta_b.mean(axis=0)
+                    self.W += lr * delta_W
+                    self.a += lr * delta_a
+                    self.b += lr * delta_b
 
-                self.W += lr * delta_W
-                self.a += lr * delta_a
-                self.b += lr * delta_b
-
-            eqm = torch.abs((v_0 - v_1)).mean()
-            eqm_list.append(eqm.cpu())
-            if plot_error is True:
-                print(f"============= Epoch n°{e} =============\n MAE = {eqm}")
-        if plot_error is True:
+                acc = torch.abs((v0 - v1)).mean()
+                acc_list.append(acc.cpu())
+                pbar.set_postfix(acc=f"{acc:.4f}")
+                pbar.update(1) 
+        if verbose == 2:
             plt.figure()
-            plt.plot(eqm_list)
-            plt.ylabel('MAE')
+            plt.plot(acc_list)
+            plt.ylabel('Accuracy')
             plt.xlabel('Epoch')
             plt.grid()
             plt.show()
             
     def generer_image_RBM(self, iterations_gibbs, nb_images, show=False):
         # initialisation donnees v
-        v = torchnp(np.random.random_sample((nb_images, 1, self.p))).to(device)
+        v = torch.from_numpy(np.random.random_sample((nb_images, 1, self.p))).to(device)
         v = torch.round(v)
         for i in range(iterations_gibbs):
             tirage_h = self.entree_sortie_RBM(v)  # (n, 1, q)
-            h = bernoulli(tirage_h)
+            h = torch.bernoulli(tirage_h)
             # Tirage de v (taille p x 1) dans loi p(v|h^0)
             tirage_v = self.sortie_entree_RBM(h)  # (n, 1, p)
-            v = bernoulli(tirage_v)
+            v = torch.bernoulli(tirage_v)
         list_img =[]
         for img in range(nb_images):
             X = np.reshape(v[img].cpu().flatten(), (20, 16))
